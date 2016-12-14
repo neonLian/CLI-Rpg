d3f1566c315fa6796a1f1d09b5b8553af609775a
RustyChainzii@gmail.com <RustyChainzii@gmail.com>
2016-12-13 17:32:54 -1000

    im adding a player class fell free to change

---
+++ Player.py
@@ -0,0 +1,22 @@
+
+
+
+class Player():
+	def __init__(self):
+	self.HP = 100
+	self.Damage = 10
+	#name = player till the player gives its name
+	self.name = "Player"
+	self.mana = 20
+	self.weapon = "na"
+	self.level = 1
+	#cur majic is the spell you want to equip
+	self.Cur_magic = "na"
+	#class would be passed in
+	self.Class = "na"
+	#player state could be used for is burning or if you have an efect like a poition or spell
+	self.Player_state = "na"
+
+	
+	
+	
